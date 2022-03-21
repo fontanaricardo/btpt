c7a8a54507208658e1f6c6a0bde1e81b92179f69
Ricardo Lopes Bez Fontana <ricardo.fontana@becomex.com.br>
2022-03-21 12:23:59 -0300

    Created basic help.

--- src/Btpt.App/CommandLineOptions.cs
+++ src/Btpt.App/CommandLineOptions.cs
@@ -1,4 +1,5 @@
 using CommandLine;
+using CommandLine.Text;
 
 namespace Btpt.App
 {
@@ -21,5 +22,25 @@ namespace Btpt.App
 
         [Option(shortName: 'o', longName: "longitude", Required = false, HelpText = "The current longitude position.", Default = 0)]
         public double Longitude { get; set; }
+
+        [Usage(ApplicationAlias = "btpt")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                new Example("Tool to punch the clock, only the cingo provider are supported now.",
+                    new CommandLineOptions
+                    {
+                        Provider = "cingo",
+                        UserName = "00000000000",
+                        Password = "password",
+                        TimeZone = 3,
+                        Latitude = -23.703760,
+                        Longitude = -46.700920 
+                    })
+                };
+            }
+        }
     }
 }
